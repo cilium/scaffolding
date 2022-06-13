f1790cffa0ac9ae84967d85378cd09d24edfac91
Ryan Drew <ryan.drew@isovalent.com>
2022-06-13 11:52:41 -0600

    Simplify circleci.py script
    Signed-off-by: Ryan Drew <ryan.drew@isovalent.com>

--- ci/circleci.py
+++ ci/circleci.py
@@ -25,125 +25,90 @@ MATRIX_PATH: Path = ROOT_PATH / "matrix.jsonnet"
 VARS_DIR_PATH: str = "vars"
 
 
-class TemplateHandler:
-    """Handle rendering of jinja2 templates"""
+def render_and_write_templates(output_path: Path, **kwargs) -> None:
+    """
+    Render templates in `TEMPLATES_PATH` and write result to disk.
 
-    def __init__(self):
-        self.loader = jinja2.FileSystemLoader(TEMPLATES_PATH)
-        self.environment = jinja2.Environment(
-            loader=self.loader,
+    Parameters
+    ----------
+    output_path : Path
+        Directory to output rendered templates to.
+    kwargs
+        Any kwargs are passed to templates as variables.
+    """
+
+    environment = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(TEMPLATES_PATH)
+    )
+
+    templates = environment.list_templates()
+    logging.info("Rendering the following templates: %s", ", ".join(templates))
+    logging.info(
+        "Using the following context: %s", pprint.pformat(**kwargs, indent=4)
+    )
+    for template_name in templates:
+        logging.debug("Rendering %s", template_name)
+        template = environment.get_template(template_name)
+        result = template.render(**kwargs)
+        logging.debug(
+            "Rendered %s as %s",
+            template_name,
+            result,
         )
-        self.context: t.Dict[str, str] = {}
-        self.rendered: t.Dict[str, str] = {}
-
-    def add_vars(self, **kwargs):
-        """Add variables into the context for jinja2 evaluation."""
-        self.context.update(**kwargs)
-
-    def render_all(self):
-        """
-        Render all templates in `TEMPLATES_PATH`.
-
-        Results will be written to `output_path`, using the same
-        filename as the templates but with the `.j2` extension removed.
-        For instance, if a template named `mytemplate.yml.j2` is
-        rendered, it will be placed into the `output_path` as
-        `mytemplate.yml`
-        """
-        templates = self.environment.list_templates()
-        logging.info("Rendering the following templates: %s", ", ".join(templates))
-        logging.info(
-            "Using the following context: %s", pprint.pformat(self.context, indent=4)
-        )
-        for template_name in templates:
-            logging.debug("Rendering %s", template_name)
-            template = self.environment.get_template(template_name)
-            result = template.render(**self.context)
-            logging.debug(
-                "Rendered %s as %s",
+        template_output_name = template_name.removesuffix(".j2")
+        with open(
+            output_path / template_output_name, "w", encoding="utf-8"
+        ) as to_file_handler:
+            logging.info(
+                "Writing rendered template '%s' to '%s'",
                 template_name,
-                result,
+                template_output_name,
             )
-            self.rendered[template_name] = result
-
-    def write_all(self, output_path: Path):
-        """
-        Write all rendered templates to the given `output_path`.
-
-        Results will be written to `output_path`, using the same
-        filename as the templates but with the `.j2` extension removed.
-        For instance, if a template named `mytemplate.yml.j2` is
-        rendered, it will be placed into the `output_path` as
-        `mytemplate.yml`
-        """
-
-        for template_name, result in self.rendered.items():
-            template_output_name = template_name.removesuffix(".j2")
-            with open(
-                output_path / template_output_name, "w", encoding="utf-8"
-            ) as to_file_handler:
-                logging.info(
-                    "Writing rendered template '%s' to '%s'",
-                    template_name,
-                    template_output_name,
-                )
-                to_file_handler.write(result)
+            to_file_handler.write(result)
 
 
-class MatrixHandler:
-    """Handle loading testing matrix jsonnet file"""
+def render_and_write_matrix(output_path: Path, **kwargs) -> t.Dict[str, str]:
+    """
+    Render testing matrix jsonnet file at `MATRIX_PATH`.
 
-    def __init__(self):
-        self.matrix: t.List[t.Dict[str, str]] = []
-        self.matrix_str: t.Optional[str] = None
-        self.loaded: bool = False
-        self.ext_vars: t.Dict[str, str] = {}
-        self.scenarios: t.Dict[str, str] = {}
+    Parameters
+    ----------
+    output_path : Path
+        Path to write the rendered matrix to.
+    kwargs
+        Any kwargs given will be used as external variables for
+        rendering
+    
+    Returns
+    -------
+    Scenarios pulled from testing matrix, as map from the scenario's
+    name to its variables.
+    """
 
-    def add_vars(self, **kwargs):
-        """Add 'external variables' to the jsonnet evaluation."""
-        self.ext_vars.update(**kwargs)
+    logging.info("Loading testing matrix")
+    matrix_str = jsonnet.evaluate_file(
+        str(MATRIX_PATH), ext_vars=kwargs
+    )
+    matrix = json.loads(matrix_str)
+    logging.info(
+        "Got the following testing matrix:\n%s", pprint.pformat(matrix)
+    )
 
-    def load(self):
-        """
-        Read and evaluate testing matrix jsonnet file.
+    matrix_out_path = output_path / MATRIX_OUTPUT_NAME
+    with open(matrix_out_path, "w", encoding="utf-8") as matrix_file_handler:
+        logging.info("Writing rendered testing matrix to '%s'", matrix_out_path)
+        matrix_file_handler.write(matrix_str)
 
-        File will be pulled from `MATRIX_PATH`.
-        """
-        logging.info("Loading testing matrix")
-        self.matrix_str = jsonnet.evaluate_file(
-            str(MATRIX_PATH), ext_vars=self.ext_vars
-        )
-        self.matrix = json.loads(self.matrix_str)
-        logging.info(
-            "Got the following testing matrix:\n%s", pprint.pformat(self.matrix)
-        )
-        self.scenarios = {scenario["_name"]: json.dumps(scenario) for scenario in self.matrix}
-        self.loaded = True
-
-    def write(self, output_path: Path):
-        """
-        Write evaluated matrix to the given output directory.
-
-        Resulting file will be named `MATRIX_OUTPUT_NAME`.
-        """
-        if not self.loaded:
-            self.load()
-        if self.matrix_str is None:
-            raise ValueError(
-                "Unable to write testing matrix as it hasn't been loaded yet"
-            )
-        matrix_out_path = output_path / MATRIX_OUTPUT_NAME
-        with open(matrix_out_path, "w", encoding="utf-8") as matrix_file_handler:
-            logging.info("Writing testing matrix to '%s'", matrix_out_path)
-            matrix_file_handler.write(self.matrix_str)
-        for scenario, varfile in self.scenarios.items():
-            varfile_output_path = output_path / VARS_DIR_PATH / (scenario + ".yml")
-            with open(
-                varfile_output_path, "w", encoding="utf-8"
-            ) as varfile_file_handler:
-                logging.info("Writing varfile '%s'", varfile_output_path)
-                varfile_file_handler.write(varfile)
+    scenarios = {scenario["_name"]: json.dumps(scenario) for scenario in matrix}
+    for scenario, varfile in scenarios.items():
+        varfile_output_path = output_path / VARS_DIR_PATH / (scenario + ".yml")
+        with open(
+            varfile_output_path, "w", encoding="utf-8"
+        ) as varfile_file_handler:
+            logging.info("Writing varfile '%s'", varfile_output_path)
+            varfile_file_handler.write(varfile)
+    
+    return scenarios
 
 
 def get_argument_parser() -> argparse.ArgumentParser:
@@ -178,23 +143,18 @@ def main():
     # Create directories in case they don't exist
     (output_dir / VARS_DIR_PATH).mkdir(parents=True, exist_ok=True)
 
-    matrix_handler = MatrixHandler()
-    matrix_handler.load()
-    matrix_handler.write(output_dir)
-    scenario_list = list(matrix_handler.scenarios.keys())
+    scenarios = render_and_write_matrix(output_dir)
 
     if args.just_varfiles:
         return
 
-    template_handler = TemplateHandler()
-    template_handler.add_vars(
+    scenario_list = list(scenarios.keys())
+    render_and_write_templates(
+        output_dir,
         build_image=args.build_image,
         scenarios=scenario_list,
         vars_dir=VARS_DIR_PATH,
     )
-    template_handler.render_all()
-    template_handler.write_all(output_dir)
-    return
 
 
 if __name__ == "__main__":
