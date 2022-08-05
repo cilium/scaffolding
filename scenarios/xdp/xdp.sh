3578dd589f54310c475448ce8c95cb0e26668bfc
Ryan Drew <ryan.drew@isovalent.com>
2022-08-05 15:25:33 -0600

    Add metallb for xdp test, fix netperf 0 t/s
    Rather than rely on minikube tunnel, use metallb. Not exactly sure
    how we can measure the impact of xdp acceleration with dsr on cpu,
    but TDB.
    
    Additionally fixes an issue with the netperf test, as I was
    getting a consistent 0 transactions / second. TIL netperf uses port
    12865 to establish a connection, then uses another port to
    transfer data. The k8s service was only setup to allow for traffic
    coming into the 'connection control' port. I added another
    explicit port in the service, 12866, for data transfer.
    
    Another aspect to this commit is adding in the '-k' arg to netperf
    for printing additional stats. This was taken from
    cilium/kubenetbench, thank you!
    
    Signed-off-by: Ryan Drew <ryan.drew@isovalent.com>

--- scenarios/xdp/xdp.sh
+++ scenarios/xdp/xdp.sh
@@ -19,16 +19,15 @@ build_toolkit
 CILIUM_VERSION=1.11.6  # target cilium version to install
 
 MONITORING_NODE="minikube"  # where monitoring tools will be deployed
-SERVER_NODE="minikube-m03"  # where netperf server is deployed
-SVC_NODE="minikube"  # where our netperf client will target
+SERVER_NODE="minikube-m02"  # where netperf server is deployed
+
 # label above nodes with correct scaffolding roles to
 # satisfy node selectors from kustomize templates
 label_nodes() {
+    kubectl label nodes --all role.scaffolding/monitored=true
     kubectl label node $MONITORING_NODE role.scaffolding/monitoring=true
     kubectl label node $SERVER_NODE role.scaffolding/pod2pod-server=true
-    # we won't use this client, but it needs to go somewhere
-    kubectl label node $SERVER_NODE role.scaffolding/pod2pod-client=true
-    kubectl label node $SERVER_NODE role.scaffolding/monitored=true
+
 }
 
 SSH="minikube ssh -n"  # how to ssh into nodes
@@ -40,8 +39,11 @@ delete_kube_proxy() {
     kubectl -n kube-system delete cm kube-proxy
     set -e
 
-    for node in $MONITORING_NODE $CLIENT_NODE $SERVER_NODE
+    minikubedt=$(date -u)
+
+    for node in "minikube" "minikube-m02"
     do
+        $SSH $node "sudo date --set=\"$minikubedt\""
         $SSH $node "sudo /bin/bash -c 'iptables-save | grep -v KUBE | iptables-restore'"
         $SSH $node "sudo /bin/bash -c 'echo '"'"'net.ipv4.conf.*.rp_filter = 0'"'"' > /etc/sysctl.d/99-override_cilium_rp_filter.conf && systemctl restart systemd-sysctl'"
     done
@@ -99,17 +101,19 @@ wait_cilium_ready() {
 # run netperf against svc/pod2pod-server, which is a nodePort service
 run_netperf() {
     ip=$(kubectl get svc pod2pod-server -ojsonpath='{.status.loadBalancer.ingress[0].ip}')
-    # defined in the manifest, our nodeport target is 30000
 
     # -H: target ip
     # -t: test type
     # -l: length of test
-    # -j: keep additional test stats
+    # -j: keep additional testtats
     netperf \
         -H $ip \
         -t TCP_STREAM \
         -l 60s \
-        -j
+        -j \
+        -- \
+        -P 12866 \
+        -k THROUGHPUT,THROUGHPUT_UNITS,THROUGHPUT_CONFID,PROTOCOL,ELAPSED_TIME,LOCAL_SEND_CALLS,LOCAL_BYTES_PER_SEND,LOCAL_RECV_CALLS,LOCAL_BYTES_PER_RECV,REMOTE_SEND_CALLS,REMOTE_BYTES_PER_SEND,REMOTE_RECV_CALLS,REMOTE_BYTES_PER_RECV,LOCAL_SYSNAME,LOCAL_RELEASE,LOCAL_VERSION,LOCAL_MACHINE,REMOTEL_SYSNAME,REMOTEL_RELEASE,REMOTEL_VERSION,REMOTEL_MACHINE,COMMAND_LINE,LOCAL_TRANSPORT_RETRANS,REMOTE_TRANSPORT_RETRANS,TRANSACTION_RATE,P50_LATENCY,P90_LATENCY,RT_LATENCY,MEAN_LATENCY,STDEV_LATENCY,REQUEST_SIZE,RESPONSE_SIZE,BURST_SIZE
 }
 
 # Get cilium cli for status
@@ -125,12 +129,14 @@ fi
 # without having to recreate the cluster
 if ! [ "$(kubectl config current-context)" == "minikube" ]
 then
-    minikube start --driver=kvm2 --nodes=3 --cni=false --network-plugin=cni
+    minikube start --driver=kvm2 --nodes=2 --cni=false --network-plugin=cni --addons=metallb
     wait_ready
+    kubectl apply -f ./metallb_l2.yaml
     label_nodes
     prep_for_cilium
 else
     wait_ready
+    kubectl apply -f ./metallb_l2.yaml
     set +e
     label_nodes
     set -e
@@ -155,7 +161,8 @@ kustomize build . > $ARTIFACTS/manifest.yaml
 
 # Why create || replace?
 # See https://github.com/prometheus-community/helm-charts/issues/1500
-kubectl create -f $ARTIFACTS/manifest.yaml || kubectl replace -f $ARTIFACTS/manifest.yaml
+# the || true is there when we have an existing pvc for grafana
+kubectl create -f $ARTIFACTS/manifest.yaml || kubectl replace -f $ARTIFACTS/manifest.yaml || true
 
 # Wait for everything to show ready
 wait_ready
@@ -164,6 +171,5 @@ wait_ready
 # For instance, during this time you can pull up grafana
 # kubectl port-forward svc/grafana -n monitoring 3000:3000
 # admin:admin
-# If running on minikube, need to do a `minikube tunnel` call
 breakpoint
 run_netperf
