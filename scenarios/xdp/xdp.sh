83a847f5916e97029eff1a9bb71d130a4599614d
Ryan Drew <ryan.drew@isovalent.com>
2022-08-08 21:06:22 -0600

    Fix XDP install, add no kpr test, pin lb to node
    With this commit a couple of changes are introduced:
    
    1. Create a `netperf.sh` which kicks off the netperf client. This
       was done to allow for running netperf outside of the `xdp.sh`
       script as needed.
    2. Configure metallb to use a single, dedicated node for l2
       advertising. This forces any traffic to the netperf server
       to funnel through a dedicated node which we can monitor the
       cpu usage of over time. A verification step is introduced
       which checks that the mac address for the netperf server
       matches the mac address of the dedicated lb node. This change
       also has metallb installed outside of minikube, as the
       minikube addon is pretty outdated.
    3. Introduce another test which has a basic installation of
       cilium, without kube-proxy replacement. This will help us
       get a good baseline of what we can expect with kube-proxy.
       In the future, the kube-proxy replacement test (no XDP) will
       have DSR enabled, so we can compare kube-proxy replacement with
       DSR to kube-proxy replacement with DSR **with** XDP
       acceleration.
    4. Fixes a failing install of cilium with XDP acceleration on
       minikube. When minikube creates vms, it uses a queue size
       of zero for its nics (see [1]). This queue size corresponds
       to the channels of a network device, as displayed in
       `ethtool -l` and as required by cilium during install with
       XDP acceleration enabled. When creating a new minikube
       cluster, the vm xml is edited to set this queue property.
       This adds startup time to each of the tests (about 1-2 minutes),
       but is required.
    
    [1]: https://libvirt.org/formatdomain.html#setting-nic-driver-specific-options
    
    Signed-off-by: Ryan Drew <ryan.drew@isovalent.com>

--- scenarios/xdp/xdp.sh
+++ scenarios/xdp/xdp.sh
@@ -16,63 +16,120 @@ fi
 # k8s pods and nodes are ready
 build_toolkit
 
-CILIUM_VERSION=1.11.6  # target cilium version to install
+# get cilium cli for install/status
+if ! [ -f "$ARTIFACTS/cilium" ]
+then
+    cd $ARTIFACTS
+    $SCRIPT/get_ciliumcli.sh
+    cd $SCENARIO_DIR
+fi
 
+# -- variables --
+# anything in all caps is used in a global scope
+CILIUM_VERSION=1.11.6  # target cilium version to install
+CONTROL_PLANE_NODE="minikube"  # where k8s api server is running
 MONITORING_NODE="minikube"  # where monitoring tools will be deployed
 SERVER_NODE="minikube-m02"  # where netperf server is deployed
+LB_NODE="minikube-m03"  # where our metallb loadbalancer will be pinned
+ALL_NODES=("minikube" "minikube-m02" "minikube-m03")  # list of nodes to work with
+SSH="minikube ssh -n"  # how to ssh into nodes
+# these three are set at runtime
+API_SERVER_IP=""  # ip of api server
+API_SERVER_PORT=""  # port of api server
+CLUSTER_CIDR=""  # cluster cidr of the cluster
 
-# label above nodes with correct scaffolding roles to
+# -- functions --
+# create a new three node minikube cluster using kvm
+# sets queue length for each interface to 4 by editing each node's
+# xml and doing a reboot
+# see https://marc.info/?l=xdp-newbies&m=157431975621734&w=2
+new_minikube() {
+    # assume 2 cpus per node
+    # if that changes, adjust the queues below to be twice the number of cpus
+    # per node
+    minikube start --driver=kvm2 --nodes=3 --cni=false --network-plugin=cni
+    for node in ${ALL_NODES[@]}
+    do
+        virsh --connect qemu:///system dumpxml $node > $ARTIFACTS/$node.xml
+        sed -i 's/<interface type='"'"'network'"'"'>/<interface type='"'"'network'"'"'>\n<driver name='"'"'vhost'"'"' queues='"'"'4'"'"'\/>/g' artifacts/$node.xml
+        virsh --connect qemu:///system define $ARTIFACTS/$node.xml
+        virsh --connect qemu:///system shutdown $node
+    done
+    # wait until all nodes are down
+    $SCRIPT/retry.sh 4 '[ ! "$(virsh --connect qemu:///system list | grep -i minikube)" ]'
+    minikube start
+}
+
+# set correct date for each node and diasble rp_filter accross the board
+node_prep() {
+    now=$(date -u)
+    for node in ${ALL_NODES[@]}
+    do
+        $SSH $node "sudo date --set=\"$now\""
+        $SSH $node "sudo /bin/bash -c 'echo '"'"'net.ipv4.conf.*.rp_filter = 0'"'"' > /etc/sysctl.d/99-override_cilium_rp_filter.conf && systemctl restart systemd-sysctl'"
+    done
+}
+
+# label nodes with correct scaffolding roles to
 # satisfy node selectors from kustomize templates
 label_nodes() {
     kubectl label nodes --all role.scaffolding/monitored=true
     kubectl label node $MONITORING_NODE role.scaffolding/monitoring=true
     kubectl label node $SERVER_NODE role.scaffolding/pod2pod-server=true
-
+    kubectl label node $LB_NODE role.scaffolding/lb=true
 }
 
-SSH="minikube ssh -n"  # how to ssh into nodes
 # minikube installs kube-proxy by default, this function removes it
 # see https://docs.cilium.io/en/v1.9/gettingstarted/kubeproxy-free/
 delete_kube_proxy() {
     set +e
+    kubectl -n kube-system get ds kube-proxy -o yaml > $ARTIFACTS/kube-proxy-ds.yaml
     kubectl -n kube-system delete ds kube-proxy
-    kubectl -n kube-system delete cm kube-proxy
+    kubectl -n kube-system get cm kube-proxy -o yaml > $ARTIFACTS/kube-proxy-cm.yaml
+    kubectl -n kube-system delete cm kube-proxy 
     set -e
 
-    minikubedt=$(date -u)
-
-    for node in "minikube" "minikube-m02"
+    for node in ${ALL_NODES[@]}
     do
-        $SSH $node "sudo date --set=\"$minikubedt\""
         $SSH $node "sudo /bin/bash -c 'iptables-save | grep -v KUBE | iptables-restore'"
-        $SSH $node "sudo /bin/bash -c 'echo '"'"'net.ipv4.conf.*.rp_filter = 0'"'"' > /etc/sysctl.d/99-override_cilium_rp_filter.conf && systemctl restart systemd-sysctl'"
     done
 }
 
-API_SERVER_IP=""
-API_SERVER_PORT=""
-# delete kube proxy, grap api server ip and port
-prep_for_cilium() {
-    delete_kube_proxy
-    API_SERVER_IP="$(kubectl get node minikube -o jsonpath='{.status.addresses[0].address}')"
-    API_SERVER_PORT="8443"
+# ensure that cilium is installed with KubeProxyReplacement set to strict
+# verifies by using cilium status on a node
+# see https://docs.cilium.io/en/v1.12/gettingstarted/kubeproxy-free/#validate-the-setup
+ensure_strict_kpr() {
+    kubectl -n kube-system exec ds/cilium -- cilium status | grep KubeProxyReplacement | grep Strict
 }
 
-# install cilium using the cilium cli for a non-xdp deployment
-install_cilium_no_xdp() {
+# basic install of cilium, no kube-proxy replacement (kpr), dsr, xdp
+install_cilium_no_kpr() {
+    $ARTIFACTS/cilium install \
+        --version=$CILIUM_VERSION
+}
+
+# install cilium with kube-proxy replacement (kpr), no dsr or xdp
+# deletes kube-proxy if it exists in the cluster
+install_cilium_kpr_no_xdp() {
+    delete_kube_proxy
+    wait_ready
     $ARTIFACTS/cilium install \
         --version=$CILIUM_VERSION \
         --helm-set endpointRoutes.enabled=true \
         --helm-set kubeProxyReplacement=strict \
         --helm-set k8sServiceHost=${API_SERVER_IP} \
         --helm-set k8sServicePort=${API_SERVER_PORT}
+    ensure_strict_kpr
 }
 
-# install cilium using the cilium cli for a xdp deployment
-# to be precise, enables dsr with xdp acceleration
-install_cilium_xdp() {
+# install cilium with kub-proxy replacement (kpr), dsr and xdp acceleration
+# deletes kube-proxy if it exists in the cluster
+install_cilium_kpr_xdp() {
+    delete_kube_proxy
+    wait_ready
     $ARTIFACTS/cilium install \
         --version=$CILIUM_VERSION \
+        --helm-set ipv4NativeRoutingCIDR=${CLUSTER_CIDR} \
         --helm-set endpointRoutes.enabled=true \
         --helm-set kubeProxyReplacement=strict \
         --helm-set k8sServiceHost=${API_SERVER_IP} \
@@ -81,15 +138,14 @@ install_cilium_xdp() {
         --helm-set autoDirectNodeRoutes=true \
         --helm-set loadBalancer.mode=dsr \
         --helm-set loadBalancer.acceleration=native
+    ensure_strict_kpr
 }
 
-# wait for cilium to be ready, ensure KubeProxyReplacement is strict
-# run a connectivity test
+# wait for cilium to be ready and then run a connectivity test
 wait_cilium_ready() {
     wait_ready
     $ARTIFACTS/cilium status --wait --wait-duration=1m
-    # Verify
-    kubectl -n kube-system exec ds/cilium -- cilium status | grep KubeProxyReplacement | grep Strict
+
 
     if [ "$SKIP_CT" != "skip-ct" ]
     then   
@@ -98,63 +154,80 @@ wait_cilium_ready() {
     fi
 }
 
-# run netperf against svc/pod2pod-server, which is a nodePort service
-run_netperf() {
-    ip=$(kubectl get svc pod2pod-server -ojsonpath='{.status.loadBalancer.ingress[0].ip}')
-
-    # -H: target ip
-    # -t: test type
-    # -l: length of test
-    # -j: keep additional testtats
-    netperf \
-        -H $ip \
-        -t TCP_STREAM \
-        -l 60s \
-        -j \
-        -- \
-        -P 12866 \
-        -k THROUGHPUT,THROUGHPUT_UNITS,THROUGHPUT_CONFID,PROTOCOL,ELAPSED_TIME,LOCAL_SEND_CALLS,LOCAL_BYTES_PER_SEND,LOCAL_RECV_CALLS,LOCAL_BYTES_PER_RECV,REMOTE_SEND_CALLS,REMOTE_BYTES_PER_SEND,REMOTE_RECV_CALLS,REMOTE_BYTES_PER_RECV,LOCAL_SYSNAME,LOCAL_RELEASE,LOCAL_VERSION,LOCAL_MACHINE,REMOTEL_SYSNAME,REMOTEL_RELEASE,REMOTEL_VERSION,REMOTEL_MACHINE,COMMAND_LINE,LOCAL_TRANSPORT_RETRANS,REMOTE_TRANSPORT_RETRANS,TRANSACTION_RATE,P50_LATENCY,P90_LATENCY,RT_LATENCY,MEAN_LATENCY,STDEV_LATENCY,REQUEST_SIZE,RESPONSE_SIZE,BURST_SIZE
+# install metallb l2 load balancer
+# this probably won't apply to all environments, so adjust if needed
+install_metallb() {
+    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.4/config/manifests/metallb-native.yaml
+    wait_ready
+    kubectl apply -f $SCENARIO_DIR/metallb_l2.yaml
 }
 
-# Get cilium cli for status
-if ! [ -f "$ARTIFACTS/cilium" ]
-then
-    cd $ARTIFACTS
-    $SCRIPT/get_ciliumcli.sh
-    cd $SCENARIO_DIR
-fi
+# for an l2 lb, this verifies that the mac address of the pod2pod-server ip
+# matches the mac address of the internal ip for LB_NODE
+# this verifies that traffic to pod2pod-server will only pass through LB_NODE
+verify_lb_pinned_l2() {
+    lb_ip=$($SCRIPT/get_node_internal_ip.sh $LB_NODE)
+    svc_ip=$(kubectl get svc pod2pod-server -ojsonpath='{.status.loadBalancer.ingress[0].ip}')
+    # use ping to force host to lookup mac addrs
+    for ip in $lb_ip $svc_ip
+    do
+        ping -c1 -W1 $ip || true
+    done
+
+    arp_info=$(ip neigh)
+    lb_mac=$(echo "$arp_info" | grep "$lb_ip" | awk '{print $5}')
+    svc_mac=$(echo "$arp_info" | grep "$svc_ip" | awk '{print $5}')
+
+    if [ "$lb_mac" != "$svc_mac" ]
+    then
+        echo "cannot continue, as mac addr for pod2pod-server is not pinned to $LB_NODE"
+        exit 1
+    fi
+}
 
 # Setup minikube env
 # idempodent, so we can have multiple runs of this script
 # without having to recreate the cluster
 if ! [ "$(kubectl config current-context)" == "minikube" ]
 then
-    minikube start --driver=kvm2 --nodes=2 --cni=false --network-plugin=cni --addons=metallb
+    new_minikube
     wait_ready
-    kubectl apply -f ./metallb_l2.yaml
+    node_prep
+    wait_ready
     label_nodes
-    prep_for_cilium
 else
     wait_ready
-    kubectl apply -f ./metallb_l2.yaml
     set +e
     label_nodes
     set -e
-    prep_for_cilium
 fi
 
+_api_server_url=$($SCRIPT/get_apiserver_url.sh)
+API_SERVER_IP=$(echo "$_api_server_url" | cut -d':' -f1)
+API_SERVER_PORT=$(echo "$_api_server_url" | cut -d':' -f2)
+CLUSTER_CIDR=$($SCRIPT/get_cluster_cidr.sh)
 
 # Install cilium if needed
 if ! $ARTIFACTS/cilium status --wait --wait-duration="1s"
 then
-    if [ "$1" == "nxdp" ]
+    if [ "$1" == "nkpr" ]
     then
-        install_cilium_no_xdp
+        install_cilium_no_kpr
+    elif [ "$1" == "kpr" ]
+    then
+        install_cilium_kpr_no_xdp
+    elif [ "$1" == "xdp" ]
+    then
+        install_cilium_kpr_xdp
     else
-        install_cilium_xdp
+        echo "expected one of: nkpr, kpr, xdp"
+        exit 1
     fi
 fi
 wait_cilium_ready
+
+install_metallb
+wait_ready
 
 # Deploy netperf server, monitoring tools
 kustomize build . > $ARTIFACTS/manifest.yaml
@@ -167,9 +240,12 @@ kubectl create -f $ARTIFACTS/manifest.yaml || kubectl replace -f $ARTIFACTS/mani
 # Wait for everything to show ready
 wait_ready
 
+# Insure our load balancer is setup correctly
+verify_lb_pinned_l2
+
 # Wait for user input to run netperf test
 # For instance, during this time you can pull up grafana
 # kubectl port-forward svc/grafana -n monitoring 3000:3000
 # admin:admin
 breakpoint
-run_netperf
+$SCENARIO_DIR/netperf.sh | tee -a $ARTIFACTS/netperf_$(date +%s)_"$1".txt
