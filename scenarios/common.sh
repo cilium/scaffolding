a9ad8f26ae27700554f40b4693aa1651ad818c9a
Ryan Drew <ryan.drew@isovalent.com>
2022-10-14 11:55:59 -0600

    Improve usability of scenarios/common.sh
    This commit addresses a couple of issues within
    scenarios/common.sh that impact its usability:
    
    1. Break functionality of setting environment variables and
       printing imported vars and functions into two separate
       functions. This lets common.sh be imported into non-main
       scripts without spamming the terminal with the same
       "I am importing ..." over and over again. Example:
    
    ```
    . ../common.sh
    init
    init_print
    
    ./deploy_gke.sh
    ```
    
    ```
    . ../common.sh
    init
    
    gcloud container clusters create ... | tee -a $ARTIFACTS/gke.log
    ```
    
    2. Rename `toolkitbin` to just `toolkit` as it was a bit confusing
       building `toolkit` and saving the binary to something named
       other than `toolkit`.
    3. Build `toolkit` in the `init` function, as it's a common enough
       need to have toolkit built anyways.
    
    Signed-off-by: Ryan Drew <ryan.drew@isovalent.com>

--- scenarios/common.sh
+++ scenarios/common.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-echo "importing the following from $(basename $0):"
+# init function for main scripts in scenarios, does the following:
+# - set environment variables for locations to different directories
+#   in the repo
+# - create artifacts directory
+# - ensure toolkit is in artifacts directory
+init() {
+    SCENARIO_DIR=$(pwd)
+    ROOT_DIR=$(realpath ../../)
+    TOOLKIT=$ROOT_DIR/toolkit
+    SCRIPT=$ROOT_DIR/scripts
+    IMAGE=$ROOT_DIR/images
+    KUSTOMIZE=$ROOT_DIR/kustomize
+    ARTIFACTS=$SCENARIO_DIR/artifacts
 
-SCENARIO_DIR=$(pwd)
-ROOT_DIR=$(realpath ../../)
-TOOLKIT=$ROOT_DIR/toolkit
-SCRIPT=$ROOT_DIR/scripts
-IMAGE=$ROOT_DIR/images
-KUSTOMIZE=$ROOT_DIR/kustomize
-ARTIFACTS=$SCENARIO_DIR/artifacts
+    mkdir -p $ARTIFACTS
+    if ! test -f "$ARTIFACTS/toolkit"
+    then
+        build_toolkit
+    fi
+}
 
-echo "---  env vars ---"
-cat <<EOF
+# print what is imported from this script
+init_print() {
+    cat <<EOF
+importing the following from common.sh
+--- env vars ---
 SCENARIO_DIR=$SCENARIO_DIR
 ROOT_DIR=$ROOT_DIR
 TOOLKIT=$TOOLKIT
@@ -20,32 +34,30 @@ SCRIPT=$SCRIPT
 IMAGE=$IMAGE
 KUSTOMIZE=$KUSTOMIZE
 ARTIFACTS=$ARTIFACTS
+--- functions ---
+build_toolkit
+wait_ready
+breakpoint
 EOF
+}
 
-echo "--- functions ---"
-echo "build_toolkit"
-echo "wait_ready"
-echo "breakpoint"
 
 # build toolkit bin
 build_toolkit() {
     cd $TOOLKIT
-    go build -o $ARTIFACTS/toolkitbin .
+    go build -o $ARTIFACTS/toolkit .
     cd $SCENARIO_DIR
 }
 
 # wait for cluster to be ready to go
-wait_ready () {
-    $SCRIPT/retry.sh 5 $SCRIPT/k8s-api-readyz.sh
-    $SCRIPT/retry.sh 5 $ARTIFACTS/toolkitbin verify k8s-ready
+wait_ready() {
+    $SCRIPT/retry.sh 5 $SCRIPT/k8s_api_readyz.sh
+    $SCRIPT/retry.sh 5 $ARTIFACTS/toolkit verify k8s-ready
 }
 
 
 # wait for user input
-breakpoint () {
+breakpoint() {
     read  -n 1 -p "press key to continue..."
     echo
 }
-
-mkdir -p $ARTIFACTS
-set -x
