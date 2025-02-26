a857b0ea8d429b6656615cb7391efaab7d0bcc5d
Marco Iorio <marco.iorio@isovalent.com>
2025-02-26 12:18:42 +0100

    egw-scale-utils/client: introduce stress mode
    Extend the egw-scale-utils client with a new mode that allows to assess
    the maximum number of parallel connections that can be opened from a
    client to an external destination via the egress gateway. Specifically,
    once the first connection is successfully established, the client
    continuously opens new connections until repeated failures are
    encountered. All connections are closed only once the client process
    gets terminated.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- egw-scale-utils/pkg/client.go
+++ egw-scale-utils/pkg/client.go
@@ -22,6 +22,8 @@ type ClientConfig struct {
 	ExternalTargetAddr string
 	Interval           time.Duration
 	TestTimeout        time.Duration
+	Stress             bool
+	StressDelay        time.Duration
 }
 
 var (
@@ -39,6 +41,16 @@ var (
 		Name: "egw_scale_test_failed_tests_total",
 		Help: "Incremented when a client Pod is unable to connect to the external target after a preconfigured timeout",
 	})
+
+	testStressConnectionsCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "egw_scale_test_stress_connections_total",
+		Help: "The number of connections either successfully opened or unexpectedly closed towards the external target",
+	}, []string{"operation"})
+
+	testStressConnectionLatency = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name: "egw_scale_test_stress_connection_latency_seconds",
+		Help: "The time that it takes for a new connection to be successfully opened",
+	})
 )
 
 func getHttpReadinessProbeHandler(testHasFinished *atomic.Bool) http.HandlerFunc {
@@ -49,6 +61,79 @@ func getHttpReadinessProbeHandler(testHasFinished *atomic.Bool) http.HandlerFunc
 			w.WriteHeader(500)
 		}
 	}
+}
+
+func stressExternalTarget(
+	cfg *ClientConfig,
+	testHasFinished *atomic.Bool,
+	logger *slog.Logger,
+) {
+	const (
+		dialTimeout = 5 * time.Second
+		readTimeout = 5 * time.Second
+		maxerrs     = 3
+	)
+
+	var (
+		count  uint32
+		errcnt uint32
+		dialer = net.Dialer{
+			Timeout: dialTimeout,
+			KeepAliveConfig: net.KeepAliveConfig{
+				Enable: true, Idle: 1 * time.Second,
+				Interval: 1 * time.Second, Count: 15,
+			},
+		}
+		limiter = NewLogLimiter[string]()
+	)
+
+	// Initialize the metric labels
+	testStressConnectionsCounter.WithLabelValues("open")
+	testStressConnectionsCounter.WithLabelValues("close")
+
+	logger.Info("Waiting before starting the connections stress test", "delay", cfg.StressDelay)
+	time.Sleep(cfg.StressDelay)
+
+	for errcnt < maxerrs {
+		start := time.Now()
+		conn, err := dialer.Dial("tcp4", cfg.ExternalTargetAddr)
+		elapsed := time.Since(start)
+
+		if err != nil {
+			errcnt++
+			logger.Warn("Failed to dial target", "err", err, "cnt", count, "errcnt", errcnt, "elapsed", elapsed)
+			continue
+		}
+
+		count++
+
+		testStressConnectionsCounter.WithLabelValues("open").Inc()
+		testStressConnectionLatency.Observe(elapsed.Seconds())
+
+		if _, can := limiter.CanLog("open"); can {
+			logger.Debug("Successfully dialed target", "cnt", count, "errcnt", errcnt)
+		}
+
+		if elapsed > 100*time.Millisecond {
+			logger.Warn("Dialing took more than 100ms", "cnt", count, "errcnt", errcnt, "elapsed", elapsed)
+		}
+
+		go func(conn net.Conn) {
+			// We never actually close the connections; they will be GCed when
+			// the client is closed.
+			err := readUntilError(conn)
+
+			if cnt, can := limiter.CanLog("close"); can {
+				logger.Error("Connection unexpectedly closed", "err", err, "cnt", cnt)
+			}
+
+			testStressConnectionsCounter.WithLabelValues("close").Inc()
+			conn.Close()
+		}(conn)
+	}
+
+	testHasFinished.Store(true)
+	logger.Info("Test completed", "cnt", count, "errcnt", errcnt)
 }
 
 func connectToExternalTarget(
@@ -124,12 +209,16 @@ func connectToExternalTarget(
 
 	masqueradeDelayCounter.Add(delay.Seconds())
 
+	if cfg.Stress {
+		stressExternalTarget(cfg, testHasFinished, logger)
+	}
+
 	return nil
 }
 
 func RunClient(cfg *ClientConfig) error {
 	logger := NewLogger("client").With("external-target", cfg.ExternalTargetAddr)
-	logger.Info("Starting")
+	logger.Info("Starting", "stress", cfg.Stress)
 
 	testHasFinished := &atomic.Bool{}
 	testHasFinished.Store(false)
