4653d5b97fb0af540a0e33745fc38d2fa0f2bea4
Marco Iorio <marco.iorio@isovalent.com>
2025-02-27 18:05:41 +0100

    egw-scale-utils/server: add option to keep connection open
    Extend the egw-scale-utils server with the possibility of keeping client
    connections open, rather then closing them immediately as soon as the
    reply is sent back. This mode is useful to assess the maximum number of
    parallel connections that can be opened from a client to an external
    destination via the egress gateway.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- egw-scale-utils/pkg/logging.go
+++ egw-scale-utils/pkg/logging.go
@@ -6,6 +6,7 @@ package pkg
 import (
 	"log/slog"
 	"os"
+	"sync"
 )
 
 func NewLogger(component string) *slog.Logger {
@@ -18,4 +19,23 @@ func NewLogger(component string) *slog.Logger {
 	)
 
 	return slog.New(handler).With("component", component)
+}
+
+type LogLimiter[K comparable] struct {
+	mu  sync.Mutex
+	cnt map[K]uint64
+}
+
+func NewLogLimiter[K comparable]() *LogLimiter[K] {
+	return &LogLimiter[K]{cnt: make(map[K]uint64)}
+}
+
+func (ll *LogLimiter[K]) CanLog(key K) (cnt uint64, can bool) {
+	ll.mu.Lock()
+	defer ll.mu.Unlock()
+
+	cnt = ll.cnt[key] + 1
+	ll.cnt[key] = cnt
+
+	return cnt, cnt <= 3 || cnt == 10 || cnt == 100 || cnt%1000 == 0
 }
