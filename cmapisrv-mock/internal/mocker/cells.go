cilium/scaffolding

commit 6973fa4bb3a185506d4ac8c04d6ec0301c185722

fix(deps): update all go dependencies
Manually performed a few adaptations to account for the changes between
Cilium v1.17 and v1.18.

Signed-off-by: renovate[bot] <bot@renovateapp.com>
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

cmapisrv-mock/internal/mocker/cells.go
@@ -4,6 +4,8 @@
 package mocker
 
 import (
+	"errors"
+
 	"github.com/cilium/hive/cell"
 
 	cmhealth "github.com/cilium/cilium/clustermesh-apiserver/health"
@@ -31,7 +33,16 @@ var Cell = cell.Module(
 
 	controller.Cell,
 
-	kvstore.Cell,
+	kvstore.Cell(kvstore.EtcdBackendName),
+	cell.Invoke(func(client kvstore.Client) error {
+		if !client.IsEnabled() {
+			return errors.New("KVStore client not configured, cannot continue")
+		}
+
+		return nil
+	}),
+
+	heartbeat.Enabled,
 	heartbeat.Cell,
 	cell.Provide(func(ss syncstate.SyncState) *kvstore.ExtraOptions {
 		return &kvstore.ExtraOptions{
@@ -45,7 +56,7 @@ var Cell = cell.Module(
 	syncstate.Cell,
 	cell.Provide((*mocker).HealthEndpoints),
 
-	gops.Cell(defaults.GopsPortKVStoreMesh),
+	gops.Cell(defaults.EnableGops, defaults.GopsPortKVStoreMesh),
 	cmmetrics.Cell,
 
 	cell.Provide(newRandom),

commit 8cfa811144c61532a123970fbe5246e44936e1b3

cmapisrv-mock: fix handling of the bootstrap process
The blamed commit bumped the Cilium dependency to v1.18. However, it
missed to update the handling around the bootstrap process, causing
the mocker container to turn ready immediately, instead of waiting
until the initial state has been populated to etcd, and consequently
also lowering the rate limiting settings to the runtime ones. Let's
get this fixed to restore the previous behavior.

Fixes: 6973fa4b ("fix(deps): update all go dependencies")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

cmapisrv-mock/internal/mocker/cells.go
@@ -19,6 +19,7 @@ import (
 	"github.com/cilium/cilium/pkg/kvstore"
 	"github.com/cilium/cilium/pkg/kvstore/heartbeat"
 	"github.com/cilium/cilium/pkg/kvstore/store"
+	"github.com/cilium/cilium/pkg/lock"
 )
 
 var Cell = cell.Module(
@@ -44,8 +45,9 @@ var Cell = cell.Module(
 
 	heartbeat.Enabled,
 	heartbeat.Cell,
-	cell.Provide(func(ss syncstate.SyncState) *kvstore.ExtraOptions {
-		return &kvstore.ExtraOptions{
+	cell.Provide(func() (syncstate.SyncState, kvstore.ExtraOptions) {
+		ss := syncstate.SyncState{StoppableWaitGroup: lock.NewStoppableWaitGroup()}
+		return ss, kvstore.ExtraOptions{
 			BootstrapComplete: ss.WaitChannel(),
 		}
 	}),
@@ -53,7 +55,6 @@ var Cell = cell.Module(
 
 	cmhealth.HealthAPIServerCell,
 	cell.Provide(func() types.ClusterInfo { return types.DefaultClusterInfo }),
-	syncstate.Cell,
 	cell.Provide((*mocker).HealthEndpoints),
 
 	gops.Cell(defaults.EnableGops, defaults.GopsPortKVStoreMesh),